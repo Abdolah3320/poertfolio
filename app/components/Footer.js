8c6d7e83e4496d31d5d50f08a87d043e06be9e20
Abdolah3320 <130370108+Abdolah3320@users.noreply.github.com>
2024-09-02 15:25:56 +0300

    last-v

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,8 +1,59 @@
-export default function Footer() {
+import Link from "next/link";
+
+const Footer = () => {
   return (
-    <div className="">
-      <div></div>
-    </div>
+    <footer className="bg-black text-white px-20 pt-10 pb-2">
+      <div className="max-w mx-auto w-full px-2 grid grid-cols-1 md:grid-cols-3 gap-16 md:gap-8">
+        {/* About Section */}
+        <div className="flex flex-col justify-center items-start md:col-span-2">
+          <h3 className="text-lg font-bold mb-2">About</h3>
+          <p className="text-sm">
+            Alexas Teams is a startup founded by a group of IT students based in
+            Egypt. We are a full-stack web development team specializing in
+            technologies such as React.js, Node.js, Next.js, Express.js,
+            MongoDB, Supabase, and Flask. Our expertise spans both developing
+            our own SaaS projects and delivering tailored solutions for clients.
+            If you are interested in similar projects or have new ideas
+            you&apos;d like to explore, please feel free to contact us. We would
+            be happy to discuss how we can collaborate and create something
+            great together.
+          </p>
+        </div>
+        {/* Get in Touch Section */}
+        <div className="flex flex-col justify-center items-start">
+          <h3 className="text-lg font-bold mb-2">Get in Touch</h3>
+          <p className="text-sm">
+            Email:{" "}
+            <a
+              href="https://mail.google.com/mail/?view=cm&fs=1&to=alxa0684@gmaol.com&su=Contact%20Us&body=Hello%2C%20I%20would%20like%20to%20inquire%20about..."
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+              
+            >
+              alxa0684@gmail.com
+            </a>
+          </p>
+          <p className="text-sm">
+            Phone:{" & "}Whatsapp :{" "}
+            <a
+              href="https://wa.me/201271490616?text=Hello%2C%20I%20would%20like%20to%20inquire%20about..."
+              target="_blank"
+              rel="noopener noreferrer"className="hover:underline"
+            >
+              +01271490616
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="text-center text-sm mt-4">
+        <p>
+          &copy; {new Date().getFullYear()} Alexzes Team&apos;s. All rights
+          reserved.
+        </p>
+      </div>
+    </footer>
   );
-}
-    
+};
+
+export default Footer;
--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import NumberedLine from "@/app/components/NumberedLine";
 
 export default function Home() {
   return (
-     <div className="text-3xl text-white font-bold mt-20 justify-center items-center flex flex-col">
+    <div className="text-3xl text-white font-bold mt-20 justify-center items-center flex flex-col">
       <div className="mb-20">
         <Image
           src={logo}
@@ -19,11 +19,8 @@ export default function Home() {
       <div className="max-w-7xl mx-auto w-full grid grid-cols-9 px-2">
         <div className="col-span-4 w-full h-full">
           <Itembox
-            title="Title"
-            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
-                  corporis consequuntur voluptate nulla iusto quam ut quasi, eaque
-                  quas omnis vero totam ullam, reprehenderit ratione pariatur
-                  accusamus suscipit odit nostrum?"
+            title="School in your pocket"
+            text="School in Your Pocket is a revolutionary web app designed to cater to the diverse needs of schools, students, and teachers. Our platform provides a comprehensive and user-friendly interface that streamlines communication, collaboration, and learning, making it an indispensable tool for any educational institution."
           />
         </div>
         <div className="relative col-span-1 w-full h-full flex justify-center items-center">
@@ -37,21 +34,15 @@ export default function Home() {
         </div>
         <div className="col-span-4 w-full h-full">
           <Itembox
-            title="Title"
-            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
-                  corporis consequuntur voluptate nulla iusto quam ut quasi, eaque
-                  quas omnis vero totam ullam, reprehenderit ratione pariatur
-                  accusamus suscipit odit nostrum?"
+            title="Divima"
+            text=" Divima is a cutting-edge e-commerce web app that revolutionizes the way you shop for clothing. the platform offers a unique blend of style, customization, and convenience, allowing you to create your own personalized fashion statement."
           />
         </div>
 
         <div className="col-span-4 w-full h-full">
           <Itembox
-            title="Title"
-            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
-                  corporis consequuntur voluptate nulla iusto quam ut quasi, eaque
-                  quas omnis vero totam ullam, reprehenderit ratione pariatur
-                  accusamus suscipit odit nostrum?"
+            title="....You can be here next 😉"
+            text=""
           />
         </div>
         <div className="relative col-span-1 w-full h-full flex justify-center items-center">
